bcb47120e1310ec8b0ba9324fafa51172f0cfe32
Centurion195 <Vadim-0211@yandex.ru>
2023-04-17 23:02:24 +0300

    version 2

--- Task_18.py
+++ Task_18.py
@@ -16,10 +16,15 @@ print(list)
 
 min = math.fabs(list[0] - list[-1])
 min_index = 0
+listn = []
 
-for j in range(n-1):
-    if (math.fabs(list[j] - list[-1]) < min):
+for j in range(1, n-1):
+    if (math.fabs(list[j] - list[-1]) == min):
+        listn.append(list[j])
+    elif (math.fabs(list[j] - list[-1]) < min):
         min = math.fabs(list[j] - list[-1])
         min_index = j
 
-print(list[min_index])
+listn.append(list[min_index])
+
+print(f"Близкий(-ие) по величине элемент(ы): {listn}")
